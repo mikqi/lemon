8503cbd4ac8431efea07b3ea85c1d9b30b6b4634
Adam <35331661+appalaszynski@users.noreply.github.com>
2018-06-02 10:51:18 +0200

    Update workflow

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,29 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const watch = require('gulp-watch');
+const postcss      = require('gulp-postcss');
+const sourcemaps   = require('gulp-sourcemaps');
+const cssnano = require('cssnano');
+const autoprefixer = require('autoprefixer');
+const rename = require('gulp-rename');
+
+var plugins = [
+  autoprefixer({ browsers: ['last 10 version'] }),
+  cssnano(),
+];
 
 gulp.task('sass', () => {
   return gulp.src('./src/lemon.scss')
+    .pipe(sourcemaps.init())
     .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
-    .pipe(gulp.dest('./dist'));
+    .pipe(postcss([autoprefixer({ browsers: ['last 1 version'] })]))
+    .pipe(gulp.dest('./dist')) //unminified
+    .pipe(postcss([cssnano()]))
+    .pipe(sourcemaps.write('./'))
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('./dist')) // minified
 })
 
 gulp.task('sass:watch', () => {
